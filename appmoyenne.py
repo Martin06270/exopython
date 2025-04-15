6f6153069189bf59c60e0a7c63f454955c444487
Martin06270 <martinng06@hotmail.com>
2025-04-15 16:39:44 +0200

    Moyenne

---
+++ appmoyenne.py
@@ -0,0 +1,21 @@
+moyenne = input("Entrez votre moyenne générale : ")
+
+if len(moyenne):
+    try:
+        moyenne = float(moyenne)  # Convertit en nombre
+
+        if moyenne >= 18:
+            print("Félicitations")
+        elif moyenne >= 16:
+            print("Très bien")
+        elif moyenne >= 14:
+            print("Bien")
+        elif moyenne >= 12:
+            print("Assez bien")
+        else:
+            print("Insuffisant")
+
+    except ValueError:
+        print("Veuillez entrer un nombre valide.")
+else:
+    print("Veuillez entrer une valeur.")
