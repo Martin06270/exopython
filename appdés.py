6f73afc7c92547565ce85203d0b83f3a2fd55465
Martin06270 <martinng06@hotmail.com>
2025-04-16 16:06:51 +0200

    Dés

---
+++ appdés.py
@@ -0,0 +1,15 @@
+import random
+
+def lancer_de(nombre_faces):
+    return random.randint(1, nombre_faces) 
+
+
+try:
+    nombre_faces = int(input("Entrez le nombre de faces du dés : "))
+    if nombre_faces <= 0:
+        print("Le nombre de faces doit être supérieur à 0.")
+    else:
+        resultat = lancer_de(nombre_faces)
+        print(f"Vous avez lancé un dé à {nombre_faces} faces et obtenu : {resultat}")
+except ValueError:
+    print("Veuillez entrer un nombre entier valide.")
