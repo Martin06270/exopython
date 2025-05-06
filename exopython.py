a0d46215d027e89588d558c4e4e1df6362dfb401
Martin06270 <martinng06@hotmail.com>
2025-05-06 14:38:26 +0200

    exopython modification

--- exopython.py
+++ exopython.py
@@ -3,9 +3,9 @@ import re #Importer le module pour les expressions régulier
 #Fonction pour vérifier si une chaîne de caractères est un nombre 
 
 def valider_email(email):
-    # Définir le modèle d'une adresse e-mail 
+# Définir le modèle d'une adresse e-mail 
 
-modele = r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
+    modele = r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
 return re.match(modele, email) is not None 
 
 # Demander une adresse e-mail à l'utilisateur 
--- liste.py
+++ liste.py
@@ -3,3 +3,4 @@ ma_liste = [42, 'abc', 1.1, 30]
 ma_liste[1] = 1000
 
 print(ma_liste[3]) 
+
