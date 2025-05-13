f2244538e8dbbda8f52c0cc3aa1ca4207e8dc06e
Martin06270 <martinng06@hotmail.com>
2025-05-13 15:50:25 +0200

    exopizza

--- classe.py
+++ classe.py
@@ -9,4 +9,14 @@ class Pizza:
 
     def ajouter_ingredients(self, nouvel_ingredient):
         if nouvel_ingredient == "ananas":
-            
+            raise TypeError("Les ananas ne sont pas acceptés sur cette pizza.")
+        self.ingredients.append(nouvel_ingredient)
+        self.prix = self.prix + 1
+
+    def servir(self, table):
+        print("J'amène la pizza à la table", table)
+
+    def livre(self, adresse):
+        print("Je livre la pizza à l'adresse :", adresse)
+
+    
